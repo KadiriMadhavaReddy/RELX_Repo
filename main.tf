4249a82d87d3a2d9978148cfa262f37fe7b03fe5
Chandrasr1 <mchandrareddy99@gmail.com>
2025-02-17 17:12:15 +0530

    Terraform code initial commit

---
+++ main.tf
@@ -0,0 +1,40 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet" "default" {
+  vpc_id         = data.aws_vpc.default.id
+  default_for_az = true
+}
+
+resource "aws_security_group" "web_sg" {
+  name_prefix = "web-sg-"
+  description = "Allow HTTP and SSH traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  # Allow SSH (port 22)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Open to all (modify for security)
+  }
+
+  # Allow HTTP (port 80)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
---
+++ resources.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web" {
+  ami           = "ami-04b4f1a9cf54c11d0" 
+  instance_type = "t2.micro"
+  subnet_id     = data.aws_subnet.default.id
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
