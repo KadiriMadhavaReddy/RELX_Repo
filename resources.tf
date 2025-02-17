e5daf2941037bef7c106b7d6151f2c6296076f5a
KadiriMadhavaReddy <kadirimadhava123@gmail.com>
2025-02-17 20:24:14 +0530

    errors

--- resources.tf
+++ resources.tf
@@ -1,11 +1,10 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
   ami             = var.ami_id
   instance_type   = var.instance_type
   subnet_id     = data.aws_subnet.selected.id
+  depends_on = [aws_security_group.web_sg]
   security_groups = [aws_security_group.web_sg.name]
   user_data = file("userdata.sh")
-
-  depends_on = [aws_security_group.web_sg]
 
   tags = {
     Name = "Terraform-EC2"
