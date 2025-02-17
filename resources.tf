6847da44da98c4dc238a290f628e79518e97350e
KadiriMadhavaReddy <kadirimadhava123@gmail.com>
2025-02-17 18:00:32 +0530

    Updated subnet id

--- resources.tf
+++ resources.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-04b4f1a9cf54c11d0" 
   instance_type = "t2.micro"
-  subnet_id     = data.aws_subnet.default.id
+  subnet_id     = data.aws_subnet.selected.id
   security_groups = [aws_security_group.web_sg.name]
   depends_on = [aws_security_group.web_sg]
 
